6bf902d2be42c53c9d1161b91655d1d9183b167d
vadicusrad <vadicusrad@gmail.com>
2024-07-20 17:34:30 +0300

    add fonts and color variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home(): JSX.Element {
-
-
 	return (
 		<>
 			<Head>
@@ -16,7 +11,7 @@ export default function Home(): JSX.Element {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main className={`${styles.main} ${inter.className}`}>
+			<main  >
 
 Yo
 			</main>
