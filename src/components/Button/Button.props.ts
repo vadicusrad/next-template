65382b56a613be385f58a2d08a7f3ea7bc3fa0e7
vadicusrad <vadicusrad@gmail.com>
2024-07-21 16:47:16 +0300

    add extend types to button, create two button variant

--- src/components/Button/Button.props.ts
+++ src/components/Button/Button.props.ts
@@ -1,4 +1,10 @@
-export interface ButtonProps {
+import { ButtonHTMLAttributes, DetailedHTMLProps, HTMLAttributes } from "react"
+
+export interface ButtonProps
+	extends DetailedHTMLProps<
+		HTMLAttributes<HTMLButtonElement>,
+		HTMLButtonElement
+	> {
 	children: React.ReactNode
 	appearance: "primary" | "ghost"
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,19 @@ import styles from "./Button.module.css"
 import { ButtonProps } from "./Button.props"
 import cn from "classnames"
 
-export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
-	const buttonClass = cn(styles.button, {
+export const Button = ({
+	appearance,
+	children,
+	className,
+	...props
+}: ButtonProps): JSX.Element => {
+	const buttonClass = cn(styles.button, className, {
 		[styles.primary]: appearance === "primary",
 		[styles.ghost]: appearance === "ghost",
 	})
-	return <button className={buttonClass}>{children}</button>
+	return (
+		<button {...props} className={buttonClass}>
+			{children}
+		</button>
+	)
 }
