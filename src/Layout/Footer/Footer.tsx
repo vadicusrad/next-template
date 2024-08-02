c62d978ff9af02bb2473c1e2bf26dac5ca5696b0
vadicusrad <vadicusrad@gmail.com>
2024-08-02 12:42:12 +0300

    add grid styles to layout and footer

--- src/Layout/Footer/Footer.tsx
+++ src/Layout/Footer/Footer.tsx
@@ -1,8 +1,21 @@
 import React from "react"
 import { FooterProps } from "./Footer.props"
-
+import styles from "../Layout.module.css"
+import { format } from "date-fns"
 const Footer = ({ ...props }: FooterProps): JSX.Element => {
-	return <div>Footer</div>
+	return (
+		<footer className={styles.footer} {...props}>
+			<div>
+				OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены
+			</div>
+			<a href='#' target='_blank'>
+				Пользовательское соглашение
+			</a>
+			<a href='#' target='_blank'>
+				Политика конфиденциальности
+			</a>
+		</footer>
+	)
 }
 
 export default Footer
--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { HeaderProps } from "./Header.props"
 
 const Header = ({ ...props }: HeaderProps): JSX.Element => {
-	return <div>Header</div>
+	return <div {...props}>Header</div>
 }
 
 export default Header
